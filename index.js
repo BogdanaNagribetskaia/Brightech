f2adb3e608c79ebbf9f3914594e8b0aa34680212
unknown <bohdanaaaaa@gmail.com>
2022-09-23 00:46:35 +0300

    add sections readers/slider/subscription/footer

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ burgerBtn.addEventListener('click', function () {
   nav.classList.toggle('is-active');
   this.classList.toggle('is-active');
 });
+
+// Swiper
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+
+  navigation: {
+    nextEl: '.js-slider-button-next',
+    prevEl: '.js-slider-button-prev',
+  },
+
+  // mousewheel: {
+  //   invert: true,
+  // },
+
+  // ! Responsive breakpoints
+  // breakpoints: {
+  //   480: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 20,
+  //   },
+  //   1200: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 30,
+  //   },
+
+  //   1250: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30,
+  //   },
+  // },
+});
